8eeb8e931a407e22c63d6d94d1c20342a82efefc
0003209 <zhanghucheng@suiyi.com.cn>
2018-08-28 09:06:16 +0800

    jackson date

---
+++ project-base/README.md
@@ -0,0 +1,24 @@
+说明：jackson String to Date
+
+1 . 设置日期格式
+@Bean
+    public MappingJackson2HttpMessageConverter getMappingJackson2HttpMessageConverter() {
+        MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter = new MappingJackson2HttpMessageConverter();
+        //设置日期格式
+        ObjectMapper objectMapper = new ObjectMapper();
+        SimpleDateFormat smt = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+        objectMapper.setDateFormat(smt);
+        mappingJackson2HttpMessageConverter.setObjectMapper(objectMapper);
+        //设置中文编码格式
+        List<MediaType> list = new ArrayList<MediaType>();
+        list.add(MediaType.APPLICATION_JSON_UTF8);
+        mappingJackson2HttpMessageConverter.setSupportedMediaTypes(list);
+        return mappingJackson2HttpMessageConverter;
+
+    }
+ 存在问题：
+    1、 不能支持 yyyy-MM-dd
+    2、不能支持多个格式。
+ 临时解决：
+    在类字段上加
+    @JsonFormat(pattern="yyyy-MM-dd hh:mm:ss") 自定义控制格式
--- project-base/project-base-web/src/main/java/com/project/base/web/BaseWebMvcConfiguration.java
+++ project-base/project-base-web/src/main/java/com/project/base/web/BaseWebMvcConfiguration.java
@@ -17,6 +17,7 @@ import javax.annotation.PostConstruct;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.TimeZone;
 
 @Configuration
 public class BaseWebMvcConfiguration extends WebMvcConfigurerAdapter {
@@ -41,12 +42,13 @@ public class BaseWebMvcConfiguration extends WebMvcConfigurerAdapter {
     @Bean
     public MappingJackson2HttpMessageConverter getMappingJackson2HttpMessageConverter() {
         MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter = new MappingJackson2HttpMessageConverter();
-        //设置日期格式
+
         ObjectMapper objectMapper = new ObjectMapper();
-        SimpleDateFormat smt = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+        SimpleDateFormat smt = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        smt.setTimeZone(TimeZone.getTimeZone("GMT+8:00"));
         objectMapper.setDateFormat(smt);
         mappingJackson2HttpMessageConverter.setObjectMapper(objectMapper);
-        //设置中文编码格式
+
         List<MediaType> list = new ArrayList<MediaType>();
         list.add(MediaType.APPLICATION_JSON_UTF8);
         mappingJackson2HttpMessageConverter.setSupportedMediaTypes(list);
