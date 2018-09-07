7ce2f1439fa9188b2d8c0b8ca7aa4d6b2e685886
0003209 <zhanghucheng@suiyi.com.cn>
2018-09-07 13:03:06 +0800

    修改 jackson date formatter

--- project-base/project-base-web/src/main/java/com/project/base/web/BaseWebMvcConfiguration.java
+++ project-base/project-base-web/src/main/java/com/project/base/web/BaseWebMvcConfiguration.java
@@ -2,13 +2,14 @@ package com.project.base.web;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.project.base.web.convert.StringToDateConverter;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.util.AntPathMatcher;
 import org.springframework.web.servlet.config.annotation.*;
+
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
@@ -35,22 +36,31 @@ public class BaseWebMvcConfiguration extends WebMvcConfigurerAdapter {
         registry.addConverter(new StringToDateConverter());
     }
 
-    @Bean
-    public MappingJackson2HttpMessageConverter getMappingJackson2HttpMessageConverter() {
-        MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter = new MappingJackson2HttpMessageConverter();
 
-        ObjectMapper objectMapper = new ObjectMapper();
-        SimpleDateFormat smt = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        smt.setTimeZone(TimeZone.getTimeZone("GMT+8:00"));
-        objectMapper.setDateFormat(smt);
-        mappingJackson2HttpMessageConverter.setObjectMapper(objectMapper);
+    @Override
+    public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
 
-        List<MediaType> list = new ArrayList<MediaType>();
-        list.add(MediaType.APPLICATION_JSON_UTF8);
-        mappingJackson2HttpMessageConverter.setSupportedMediaTypes(list);
-        return mappingJackson2HttpMessageConverter;
+        boolean isSetMappingJackson2HttpMessageConverter = false;
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        simpleDateFormat.setTimeZone(TimeZone.getTimeZone("GMT+08:00"));
 
+        for (HttpMessageConverter<?> httpMessageConverter : converters) {
+            if (httpMessageConverter.getClass() != MappingJackson2HttpMessageConverter.class)
+                continue;
+            MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter = (MappingJackson2HttpMessageConverter) httpMessageConverter;
+            mappingJackson2HttpMessageConverter.getObjectMapper().setDateFormat(simpleDateFormat);
+            isSetMappingJackson2HttpMessageConverter = true;
+        }
+
+        if (!isSetMappingJackson2HttpMessageConverter) {
+            MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter = new MappingJackson2HttpMessageConverter();
+            ObjectMapper objectMapper = new ObjectMapper();
+            objectMapper.setDateFormat(simpleDateFormat);
+            mappingJackson2HttpMessageConverter.setObjectMapper(objectMapper);
+            List<MediaType> list = new ArrayList<>();
+            list.add(MediaType.APPLICATION_JSON_UTF8);
+            mappingJackson2HttpMessageConverter.setSupportedMediaTypes(list);
+            converters.add(mappingJackson2HttpMessageConverter);
+        }
     }
-
-
 }
--- project-base/project-base-web/src/main/java/com/project/base/web/GlobalExceptionHandler.java
+++ project-base/project-base-web/src/main/java/com/project/base/web/GlobalExceptionHandler.java
@@ -1,9 +1,13 @@
 package com.project.base.web;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.project.base.model.CommonResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -15,6 +19,10 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.TimeZone;
 
 /**
  *  @EnableWebMvc 加此标记，覆盖springboot 默认处理异常配置
