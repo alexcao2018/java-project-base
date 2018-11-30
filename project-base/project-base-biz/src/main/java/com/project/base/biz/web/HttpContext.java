43ea894fa635dded0b3b1b5f5e676408d77077c8
0003209 <zhanghucheng@suiyi.com.cn>
2018-11-30 16:58:55 +0800

    修复获取httprequest body空的问题

--- project-base/project-base-biz/src/main/java/com/project/base/biz/web/HttpContext.java
+++ project-base/project-base-biz/src/main/java/com/project/base/biz/web/HttpContext.java
@@ -2,12 +2,31 @@ package com.project.base.biz.web;
 
 import com.project.base.common.enums.EnumHttpRequestKey;
 import com.project.base.model.net.HttpRequestInfo;
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.context.request.RequestAttributes;
 import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.util.ContentCachingRequestWrapper;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 public class HttpContext {
     public static HttpRequestInfo getHttpRequestInfo() {
         HttpRequestInfo httpRequestInfo = (HttpRequestInfo) RequestContextHolder.getRequestAttributes().getAttribute(EnumHttpRequestKey.RequestInfo.getName(), RequestAttributes.SCOPE_REQUEST);
+        ContentCachingRequestWrapper contentCachingRequestWrapper = (ContentCachingRequestWrapper) RequestContextHolder.getRequestAttributes().getAttribute(EnumHttpRequestKey.RequestWrapper.getName(), RequestAttributes.SCOPE_REQUEST);
+        if (contentCachingRequestWrapper == null || StringUtils.isNotBlank(httpRequestInfo.getBody()))
+            return httpRequestInfo;
+
+        try {
+            if (HttpMethod.POST.name().equalsIgnoreCase(httpRequestInfo.getMethod())) {
+                httpRequestInfo.setBody(IOUtils.toString(contentCachingRequestWrapper.getContentAsByteArray(), StandardCharsets.UTF_8.name()));
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
         return httpRequestInfo;
     }
 }
--- project-base/project-base-web/src/main/java/com/project/base/web/BaseController.java
+++ project-base/project-base-web/src/main/java/com/project/base/web/BaseController.java
@@ -2,13 +2,20 @@ package com.project.base.web;
 
 import com.project.base.common.enums.EnumHttpRequestKey;
 import com.project.base.model.net.HttpRequestInfo;
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.util.ContentCachingRequestWrapper;
 
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.nio.charset.StandardCharsets;
 
 /**
  * Created by zhanghc on 2017-05-08.
@@ -25,7 +32,17 @@ public class BaseController {
     public HttpServletResponse response;
 
     public HttpRequestInfo getHttpRequestInfo() {
-        HttpRequestInfo httpRequestInfo = (HttpRequestInfo) request.getAttribute(EnumHttpRequestKey.RequestInfo.getName());
+        HttpRequestInfo httpRequestInfo = (HttpRequestInfo) RequestContextHolder.getRequestAttributes().getAttribute(EnumHttpRequestKey.RequestInfo.getName(), RequestAttributes.SCOPE_REQUEST);
+        ContentCachingRequestWrapper contentCachingRequestWrapper = (ContentCachingRequestWrapper) RequestContextHolder.getRequestAttributes().getAttribute(EnumHttpRequestKey.RequestWrapper.getName(), RequestAttributes.SCOPE_REQUEST);
+        if (contentCachingRequestWrapper == null || StringUtils.isNotBlank(httpRequestInfo.getBody()))
+            return httpRequestInfo;
+        try {
+            if (HttpMethod.POST.name().equalsIgnoreCase(request.getMethod())) {
+                httpRequestInfo.setBody(IOUtils.toString(contentCachingRequestWrapper.getContentAsByteArray(), StandardCharsets.UTF_8.name()));
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
         return httpRequestInfo;
     }
 
