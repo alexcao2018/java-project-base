18a48f73865c28e3e218e872d5ba7791cd9d7029
0003209 <zhanghucheng@suiyi.com.cn>
2018-12-14 17:54:12 +0800

    从application 中获取environment.

--- project-base/project-base-rabbitmq/src/main/java/com/project/base/rabbitmq/RabbitMQBeanDefinitionRegistryPostProcessor.java
+++ project-base/project-base-rabbitmq/src/main/java/com/project/base/rabbitmq/RabbitMQBeanDefinitionRegistryPostProcessor.java
@@ -4,20 +4,14 @@ import org.apache.commons.lang3.StringUtils;
 import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitAdmin;
-import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.beans.BeansException;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.beans.factory.config.BeanDefinition;
-import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
-import org.springframework.beans.factory.support.AbstractBeanDefinition;
 import org.springframework.beans.factory.support.BeanDefinitionBuilder;
 import org.springframework.beans.factory.support.BeanDefinitionRegistry;
 import org.springframework.beans.factory.support.BeanDefinitionRegistryPostProcessor;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
@@ -26,7 +20,7 @@ import java.text.MessageFormat;
 
 @Component
 @Configuration
-public class RabbitMQBeanDefinitionRegistryPostProcessor implements ApplicationContextAware {
+public class RabbitMQBeanDefinitionRegistryPostProcessor implements BeanDefinitionRegistryPostProcessor, ApplicationContextAware {
 
     private ApplicationContext applicationContext;
 
@@ -36,12 +30,30 @@ public class RabbitMQBeanDefinitionRegistryPostProcessor implements ApplicationC
         this.applicationContext = applicationContext;
     }
 
-    private void createRabbitListenerContainerFactoryBeanDefinition(BeanDefinitionRegistry registry, String name) {
-        BeanDefinitionBuilder beanDefinitionBuilder = BeanDefinitionBuilder.genericBeanDefinition(SimpleRabbitListenerContainerFactory.class);
-        BeanDefinition beanDefinition = beanDefinitionBuilder.getRawBeanDefinition();
-        registry.registerBeanDefinition(name, beanDefinition);
+    @Override
+    public void postProcessBeanDefinitionRegistry(BeanDefinitionRegistry registry) throws BeansException {
+        Environment environment = applicationContext.getEnvironment();
+        int i = 0;
+        while (true) {
+            String flag = environment.getProperty(MessageFormat.format("spring.rabbitmq.hosts[{0}].flag", i));
+            if (StringUtils.isBlank(flag))
+                break;
+            createRabbitListenerContainerFactoryBeanDefinition(registry, RabbitMQConfig.ContainerFactoryBeanPrefix + flag);
+            createRabbitAdminBeanDefinition(registry, RabbitMQConfig.AdminPrefix + flag);
+            i++;
+        }
     }
 
+    @Override
+    public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
+
+    }
+
+    /**
+     * 创建rabbitAdmin bean definition
+     * @param registry
+     * @param name
+     */
     private void createRabbitAdminBeanDefinition(BeanDefinitionRegistry registry, String name) {
         BeanDefinitionBuilder beanDefinitionBuilder = BeanDefinitionBuilder.genericBeanDefinition(RabbitAdmin.class);
         beanDefinitionBuilder.addConstructorArgValue(new CachingConnectionFactory());
@@ -49,26 +61,14 @@ public class RabbitMQBeanDefinitionRegistryPostProcessor implements ApplicationC
         registry.registerBeanDefinition(name, beanDefinition);
     }
 
-    @Bean
-    public BeanDefinitionRegistryPostProcessor factory(final Environment environment) {
-        return new BeanDefinitionRegistryPostProcessor() {
-            @Override
-            public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
-
-            }
-
-            @Override
-            public void postProcessBeanDefinitionRegistry(BeanDefinitionRegistry registry) throws BeansException {
-                int i = 0;
-                while (true) {
-                    String flag = environment.getProperty(MessageFormat.format("spring.rabbitmq.hosts[{0}].flag", i));
-                    if (StringUtils.isBlank(flag))
-                        break;
-                    createRabbitListenerContainerFactoryBeanDefinition(registry, RabbitMQConfig.ContainerFactoryBeanPrefix + flag);
-                    createRabbitAdminBeanDefinition(registry, RabbitMQConfig.AdminPrefix + flag);
-                    i++;
-                }
-            }
-        };
+    /**
+     * 创建rabbitListener Container factory bean definition
+     * @param registry
+     * @param name
+     */
+    private void createRabbitListenerContainerFactoryBeanDefinition(BeanDefinitionRegistry registry, String name) {
+        BeanDefinitionBuilder beanDefinitionBuilder = BeanDefinitionBuilder.genericBeanDefinition(SimpleRabbitListenerContainerFactory.class);
+        BeanDefinition beanDefinition = beanDefinitionBuilder.getRawBeanDefinition();
+        registry.registerBeanDefinition(name, beanDefinition);
     }
 }
