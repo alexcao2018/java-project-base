d0f3c6fd2f1c93d42b90f5098e0f890d0b720d3c
0003209 <zhanghucheng@suiyi.com.cn>
2018-12-26 21:12:32 +0800

    增加base job 工程。

---
+++ project-base/project-base-job/src/main/java/com/project/base/job/SchedulingConfigurerConfig.java
@@ -0,0 +1,22 @@
+package com.project.base.job;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.SchedulingConfigurer;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+import org.springframework.scheduling.config.ScheduledTaskRegistrar;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SchedulingConfigurerConfig implements SchedulingConfigurer {
+
+    @Value("${job.poolSize:10}")
+    private int poolSize;
+
+    @Override
+    public void configureTasks(ScheduledTaskRegistrar taskRegistrar) {
+        ThreadPoolTaskScheduler taskScheduler = new ThreadPoolTaskScheduler();
+        taskScheduler.setPoolSize(this.poolSize);
+        taskScheduler.initialize();
+        taskRegistrar.setTaskScheduler(taskScheduler);
+    }
+}
