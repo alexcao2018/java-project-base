ee9778e8cba4baf7f48d2881b9ca3306b30cc701
0003209 <zhanghucheng@suiyi.com.cn>
2019-02-25 10:59:10 +0800

    string to date convert support 时间戳。

--- project-base/project-base-web/src/main/java/com/project/base/web/convert/StringToDateConverter.java
+++ project-base/project-base-web/src/main/java/com/project/base/web/convert/StringToDateConverter.java
@@ -1,11 +1,15 @@
 package com.project.base.web.convert;
 
+import com.project.base.web.GlobalExceptionResolver;
 import org.apache.commons.lang3.time.DateUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Component;
 
 import java.text.ParseException;
 import java.util.Date;
+import java.util.regex.Pattern;
 
 /**
  * 此converter 只在form提交的时候才启作用，
@@ -14,14 +18,26 @@ import java.util.Date;
 public class StringToDateConverter
         implements Converter<String, Date> {
 
+    private static String timestamp10 = "^[0-9]{10}$";
+    private static String timestamp13 = "^[0-9]{13}$";
+
+    private Logger logger = LoggerFactory.getLogger(StringToDateConverter.class);
+
     @Override
     public Date convert(String source) {
         Date result = null;
         try {
-            result = DateUtils.parseDate(source, new String[]{"yyyy-MM-dd HH:mm:ss.SSS","yyyy-MM-dd HH:mm:ss", "yyyy-MM-dd"});
+            if (Pattern.matches(timestamp10, source)) {
+                result = new Date(Long.parseLong(source) * 1000);
+            } else if (Pattern.matches(timestamp13, source)) {
+                result = new Date(Long.parseLong(source));
+            } else {
+                result = DateUtils.parseDate(source, new String[]{"yyyy-MM-dd HH:mm:ss.SSS", "yyyy-MM-dd HH:mm:ss", "yyyy-MM-dd"});
+            }
         } catch (ParseException e) {
-            e.printStackTrace();
+            logger.error(e.getMessage(), e);
         }
         return result;
     }
+
 }
