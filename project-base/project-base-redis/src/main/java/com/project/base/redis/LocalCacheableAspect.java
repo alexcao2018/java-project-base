8374d5d1c7afc4f9bba49d746e6093ff98650ae3
0001186 <yanyutao@suiyi.com.cn>
2019-04-11 16:48:16 +0800

    name

--- project-base/project-base-redis/src/main/java/com/project/base/redis/LocalCacheableAspect.java
+++ project-base/project-base-redis/src/main/java/com/project/base/redis/LocalCacheableAspect.java
@@ -2,6 +2,7 @@ package com.project.base.redis;
 
 import com.github.benmanes.caffeine.cache.Caffeine;
 import com.github.benmanes.caffeine.cache.LoadingCache;
+import com.project.base.redis.annotation.LocalCacheEvict;
 import com.project.base.redis.annotation.LocalCacheable;
 import org.apache.commons.lang3.StringUtils;
 import org.aspectj.lang.JoinPoint;
@@ -48,11 +49,10 @@ public class LocalCacheableAspect {
         LocalCacheable redisCacheable = method.getAnnotation(LocalCacheable.class);
         String cacheKey = getCacheKey(redisCacheable, joinPoint, method);
 
-        LoadingCache<String, Object> loadingCache = loadingCacheMap.get(method.getName());
+        LoadingCache<String, Object> loadingCache = loadingCacheMap.get(cacheKey);
         if (loadingCache != null) {
             Object obj = loadingCache.getIfPresent(cacheKey);
             if (obj != null) {
-
                 return obj;
             }
 
@@ -62,7 +62,7 @@ public class LocalCacheableAspect {
                     .maximumSize(10000)
                     .refreshAfterWrite(redisCacheable.localCacheRefreshTime(), TimeUnit.SECONDS)
                     .build((x) -> method.invoke(joinPoint.getTarget(), joinPoint.getArgs()));
-            loadingCacheMap.put(method.getName(), loadingCache);
+            loadingCacheMap.put(cacheKey, loadingCache);
         }
         try {
 
@@ -77,7 +77,7 @@ public class LocalCacheableAspect {
     @Before("pointcutCacheEvictMethod()")
     public void beforeLocalCacheEvict(JoinPoint joinPoint) {
         Method method = cacheHelper.getMethod(joinPoint);
-        LocalCacheable redisCacheable = method.getAnnotation(LocalCacheable.class);
+        LocalCacheEvict redisCacheable = method.getAnnotation(LocalCacheEvict.class);
         String cacheKey = getCacheKey(redisCacheable, joinPoint, method);
         if (StringUtils.isEmpty(cacheKey))
             return;
@@ -90,6 +90,13 @@ public class LocalCacheableAspect {
         String splitter = localCacheable.splitter();
         String cacheKey = MessageFormat.format("{0}{1}{2}", cachePrefix, splitter, cacheSuffix);
         return cacheKey;
+    }
 
+    private String getCacheKey(LocalCacheEvict localCacheable, JoinPoint joinPoint, Method method) {
+        String cachePrefix = localCacheable.cacheName();
+        String cacheSuffix = cacheHelper.getSuffixByGenerator(joinPoint, localCacheable.key(), localCacheable.keyGenerator(), method);
+        String splitter = localCacheable.splitter();
+        String cacheKey = MessageFormat.format("{0}{1}{2}", cachePrefix, splitter, cacheSuffix);
+        return cacheKey;
     }
 }
