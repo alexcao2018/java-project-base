a67f450fae7847bcb762f770c6401f47d933b882
0002708 陈孚千 <chenfuqian@suiyi.com.cn>
2019-08-07 11:20:26 +0800

    增加获取rabbitAdmins方法

--- project-base/project-base-rabbitmq/src/main/java/com/project/base/rabbitmq/RabbitMQClientManager.java
+++ project-base/project-base-rabbitmq/src/main/java/com/project/base/rabbitmq/RabbitMQClientManager.java
@@ -4,8 +4,11 @@ import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.CollectionUtils;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Configuration
 public class RabbitMQClientManager {
@@ -52,6 +55,19 @@ public class RabbitMQClientManager {
 
     public RabbitAdmin getDefaultRabbitAdmin() {
         return defaultRabbitAdmin;
+    }
+
+    /**
+     * 返回所有的rabbitAdmins 对象，便于用户创建不同城市的queue or bindings
+     *
+     * @return the list of {@link RabbitAdmin}
+     */
+    public List<? extends RabbitAdmin> rabbitAdmins(){
+        if(CollectionUtils.isEmpty(rabbitMQAdminWrapperCollection)){
+            return Collections.EMPTY_LIST;
+        } else {
+            return rabbitMQAdminWrapperCollection.stream().map(RabbitMQAdminWrapper::getRabbitAdmin).collect(Collectors.toList());
+        }
     }
 
 }
